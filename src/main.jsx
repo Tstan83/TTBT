44bfb94e8c9fc7d621807403ad7469640c255d9a
darkwraithcovenant <joseop@hotmail.com>
2023-05-21 08:40:18 -0700

    push new code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Navbar from './components/navbar'
+import Main from './components/main'
+import Exploder from './components/exploder'
+
+function App (){
+return (
+<>
+  <Exploder />
+  <Navbar />
+  <Main />
+<h1>Hello</h1>
+<p>This is a self destructing webpage!!!</p>
+</>
+)
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+//dsadsa
