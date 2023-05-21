52c84fa232376baa05f6fd9862cbc52edf6dd0f3
darkwraithcovenant <joseop@hotmail.com>
2023-05-21 09:17:21 -0700

    this

--- src/components/exploder.jsx
+++ src/components/exploder.jsx
@@ -42,7 +42,7 @@ export default function Exploder() {
         <button onClick={handleClick}>☢️ Initiate Destruction ☢️ </button>
         <img
           key={gifKey}
-          src={isGifVisible ? '/assets/explode.gif' : '/assets/static_image.png'}
+          src={isGifVisible ? '/src/assets/explode.gif' : '/assets/static_image.png'}
           alt="GIF"
           style={gifStyle}
         />
