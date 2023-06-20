84680ad7d3cfea25c84b7820bf5c9ab3cfad252b
darkwraithcovenant <joseop@hotmail.com>
2023-06-19 18:05:14 -0700

    Tailwind init

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App (){
-return (
-<>
- 
-</>
-)
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
