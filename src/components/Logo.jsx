a6296fcee572b4137e09939b8c5a1218696fa2e5
darkwraithcovenant <joseop@hotmail.com>
2023-07-04 03:33:29 -0700

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ export default function Logo() {
         return (
           <a href="/">
           <img
-            src="src/assets/images/travis-logo.png"
+            src="../assets/images/travis-logo.png"
             alt="Travis Stanley logo"
             className="h-90 w-auto sm:h-40 mx-auto"
           />
