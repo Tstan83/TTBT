8988b8558c614636ad37464da5e973c635737c58
darkwraithcovenant <joseop@hotmail.com>
2023-07-05 22:45:34 -0700

    fixed things

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./components/navbar";
 import Bio from "./components/bio";
 import Portfolio from "./components/portfolio";
-import Booking from "./components/booking";
+// import Booking from "./components/booking";
+import Booking from "./components/booking2";
 import FAQ from "./components/FAQ"
 
 const App = () => {
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,19 +2,31 @@ export default function faq (){
 
     return (
         <section id="faq" className="text-gray-400 bg-black ">
-        <div className="container px-5 py-12 mx-auto text-center lg:px-96">
+        <div className="container px-5 py-12 mx-auto  lg:px-96">
           <div className="flex flex-col w-full mb-20">
   
-            <h1 className="sm:text-4xl text-3xl mb-4 text-white">
+            <h1 className="sm:text-4xl text-3xl mb-4 text-white text-center">
               FAQ
             </h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed">
-              Question:Blah blah blah?
-            </p>
-            <p className="lg:w-2/3 mx-auto leading-relaxed">
-              Answer:Blah blah Blah!
-            </p>
-            
+            <b className="text-yellow-200">How much is a small tattoo?</b><br/> The shop minimum is $150. This minimum covers my time and supplies<br/><br/>
+
+<b className="text-yellow-200">What’s your rate?</b><br/> The shop hourly rate is $200/hour.<br/><br/>
+
+<b className="text-yellow-200">How old do I have to be to get tattooed?</b><br/> 18 with valid ID. No exceptions. <br/><br/>
+
+<b className="text-yellow-200">Do you do genital, facial, eyeball tattoos?</b><br/> Head/face,neck and hand/palm/finger tattoos can be provided if we have a strong rapport and I know you. I do not do these tattoos on just whoever. I DO NOT do any gential tattoos or eyeball tattoos on anyone, for any reason. <br/><br/>
+
+<b className="text-yellow-200">Do you do cover-ups? </b><br/> I can do them. I will only take on cover-ups if I feel like I can do it in a way that doesn’t look like an obvious cover up. <br/><br/>
+
+<b className="text-yellow-200">What forms of payment do you accept? </b><br/> At this moment cash and Venmo are the only forms of payment I accept. We have an ATM in the lobby for your convenience.<br/><br/>
+
+<b className="text-yellow-200">Do you use numbing cream?</b><br/> No. I do not use any numbing cream for any reason as it can affect the healing and overall saturation of the tattoo. It is an insurance headache as well. <br/><br/>
+
+<b className="text-yellow-200">Can I bring a friend/partner/relative to watch? </b><br/> No. Please do not bring guests with you. If they need to be present they can wait in the lobby, if there is room for them. <br/><br/>
+
+<b className="text-yellow-200">Can you do a drawing by me/friend/kid/someone who doesn’t tattoo? </b><br/> If it’s your kids drawing, that can probably be arranged. If you want someone who doesn’t tattoo and is not a professional artist to draw your tattoo,I’m not into that. I can draw your idea and prefer that over correcting someone else’s drawings like an art teacher. </p>
+       
           </div>
          
         </div>
